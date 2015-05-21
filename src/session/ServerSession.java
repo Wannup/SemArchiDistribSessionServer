d1af785ff7aa5267812b1b1237e777b8025a36f6
cfarot <charlyfarot@gmail.com>
2015-05-21 12:41:37 +0200

    change

--- src/session/ServerSession.java
+++ src/session/ServerSession.java
@@ -1,5 +1,45 @@
 package session;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ServerSession {
 
+	private Map<String, String> configs = new HashMap<String, String>();
+	private int Port;
+	
+	public ServerSession(int port){
+		try {
+			init(port);
+			run();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void run() {
+
+		try {
+			ServerSocket socket;
+			socket = new ServerSocket(Port);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+	}
+
+	private void init(int port) throws IOException {
+
+		this.Port = port;
+
+		configs = Ini.load("config.ini");
+
+		
+	}
 }
